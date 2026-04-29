0d726955946ab49c7374f6901578e8a398e22813
gituser505 <181835637+gituser505@users.noreply.github.com>
2026-04-30 01:54:15 +0200

    Update layers.py

--- src/mylib/layers.py
+++ src/mylib/layers.py
@@ -22,49 +22,31 @@ class Sampling(layers.Layer):
         return {"seed": self.seed, **super().get_config()}
 
 
-@saving.register_keras_serializable(package="library")
+@saving.register_keras_serializable(package="projectlib")
 class PeriodicPadding2D(layers.Layer):
-    def __init__(self, padding=1, **kwargs):
-        super().__init__(**kwargs)
-        self.padding = padding
-
-    def call(self, x):
-        p = self.padding
-        if p == 0: return x
-        x = ops.concatenate([x[:, -p:, :, :], x, x[:, :p, :, :]], axis=1)
-        x = ops.concatenate([x[:, :, -p:, :], x, x[:, :, :p, :]], axis=2)
-        return x
-
-    def get_config(self):
-        return {**super().get_config(), "padding": self.padding}
-
-
-@saving.register_keras_serializable(package="mylib")
-class PeriodicPadding2DStrided(layers.Layer):
-    def __init__(self, kernel_size, strides, **kwargs):
+    def __init__(self, kernel_size, strides=0, **kwargs):
         super().__init__(**kwargs)
         self.k = kernel_size
         self.s = strides
 
     def call(self, x):
-        in_dim = x.shape[1]
-        out_dim = (in_dim + self.s - 1) // self.s
-        pad_total = max(0, (out_dim - 1) * self.s + self.k - in_dim)
-        if pad_total == 0: return x
+        L = ops.shape(x)[1]
+        out_dim = (L + self.s - 1) // self.s
+        pad_total = ops.maximum(0, (out_dim - 1) * self.s + self.k - L)
         pad_beg = pad_total // 2
         pad_end = pad_total - pad_beg
-        
-        top = x[:, -pad_beg:, :, :] if pad_beg > 0 else x[:, :0, :, :]
-        bottom = x[:, :pad_end, :, :] if pad_end > 0 else x[:, :0, :, :]
+
+        top = x[:, L - pad_beg : L, :, :]
+        bottom = x[:, 0 : pad_end, :, :]
         x = ops.concatenate([top, x, bottom], axis=1)
-        
-        left = x[:, :, -pad_beg:, :] if pad_beg > 0 else x[:, :, :0, :]
-        right = x[:, :, :pad_end, :] if pad_end > 0 else x[:, :, :0, :]
+
+        left = x[:, :, L - pad_beg : L, :]
+        right = x[:, :, 0 : pad_end, :]
         x = ops.concatenate([left, x, right], axis=2)
         return x
 
     def get_config(self):
-        return {**super().get_config(), "kernel_size": self.k, "strides": self.s }
+        return {**super().get_config(), "kernel_size": self.k, "strides": self.s}
 
 
 @saving.register_keras_serializable(package="mylib")
